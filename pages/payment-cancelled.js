ff076859888b120c3a5595ac1ff01f3c53bad328
Miamiyacht <info@miamiyachting.com>
2025-04-15 07:15:20 -0400

    Update payment-cancelled.js

--- pages/payment-cancelled.js
+++ pages/payment-cancelled.js
@@ -15,6 +15,7 @@ export default function PaymentFailedPage() {
           margin-bottom: 1.5rem;
         }
         p {
+          font-family: 'Futura PT', sans-serif;
           font-weight: 300;
         }
         .container {
@@ -39,3 +40,4 @@ export default function PaymentFailedPage() {
     </>
   );
 }
+
